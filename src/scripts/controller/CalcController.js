6d7b7a0a0c5f5fbc677c4333714cbcf9424a9426
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-08 12:08:25 -0300

    feat: CalcController constructors & getters and setters added

--- src/scripts/controller/CalcController.js
+++ src/scripts/controller/CalcController.js
@@ -1,3 +1,25 @@
 class CalcController {
+
+    constructor() {
+        this._displayCalc = "0";
+        this._dataAtual;
+
+    }
+
+    get _displayCalc() {
+        return this._displayCalc;
+    }
+
+    set _displayCalc(value) {
+        this._displayCalc = value;
+    }
     
+    get _dataAtual() {
+        return this._dataAtual;
+    }
+
+    set _dataAtual(value) {
+        this._dataAtual = value;
+    }
+
 }
