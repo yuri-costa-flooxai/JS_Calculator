ebbef6d156720285003f8259a813a46a69790ef8
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-09 12:22:19 -0300

    fix: max value to calculator

--- src/scripts/controller/CalcController.js
+++ src/scripts/controller/CalcController.js
@@ -487,6 +487,14 @@ class CalcController {
 
     set displayCalc(value){
 
+        if (value.toString().length > 10) {
+
+            this.setError();
+
+            return false;
+
+        }
+
         this._displayCalcEl.innerHTML = value;
 
     }
