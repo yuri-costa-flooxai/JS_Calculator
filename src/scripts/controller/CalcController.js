ef68346ed90e2d854b201d30393c77ff335d350f
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-09 12:20:53 -0300

    feat: audio implementation.

--- src/scripts/controller/CalcController.js
+++ src/scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -58,11 +60,40 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard() {
 
         document.addEventListener('keyup', e=> {
+
+            this.playAudio();
 
             switch (e.key) {
 
@@ -327,6 +358,8 @@ class CalcController {
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
 
